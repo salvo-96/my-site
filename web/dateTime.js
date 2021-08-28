e25968417108c89ed5f634d7ff2221832504134d
salvo-96 <salvo.cavallaro.net@gmail.com>
2021-08-29 00:47:36 +0200

    evoluzione

---
+++ web/dateTime.js
@@ -0,0 +1,13 @@
+
+function getJson() {
+    var req = new XMLHttpRequest();
+
+    req.overrideMimeType("application/json");
+    req.open('GET', "https://newsapi.org/v2/top-headlines?sources=google-news-it&apiKey=2a53dc90b8cd466e86120f25a9479af7", true);
+    req.onload = function () {
+        var jsonResponse = JSON.parse(req.responseText);
+        jsonResponse.getElementById("json").innerHTML = jsonResponse.status;
+
+    };
+
+}
